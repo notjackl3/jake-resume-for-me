76f74077b12d78a42b2ac01151f1e979850cf2e6
Jack Le <jackl32482005@gmail.com>
2025-09-03 20:07:30 -0400

    Version 3.00 (added compiler data to latex, configured s3 bucket, added basic info, allowed including/excluding projects and experiences, set up environment variables)

--- backend/resume/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Education, Experience, Project, Skill, Description
-
-admin.site.register(Education)
-admin.site.register(Experience)
-admin.site.register(Project)
-admin.site.register(Skill)
-admin.site.register(Description)
